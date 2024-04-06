439bd2eaf281bf3a0d2445e52fd7cc0cab914cd6
Rahuldj <rj712@snu.edu.in>
2024-04-06 16:19:34 +0530

    Contract Moralis-NEXTPUBLIC

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,52 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 
-
+import react,{ useState,useEffect } from 'react';
+import {useMoralis} from 'react-moralis'
 const Navbar = () => {
     const { data: session } = useSession();
+    const {enableWeb3,account,isWeb3Enabled,Moralis,deactivateWeb3,isWeb3EnableLoading}=useMoralis()
+
+    useEffect(
+        ()=>{
+            if (isWeb3Enabled) return
+
+            if (typeof window!=="undefined")
+            {
+                if (window.localStorage.getItem("connected"))
+                {
+                    enableWeb3()
+                }
+            }
+            console.log(isWeb3Enabled)
+        },[isWeb3Enabled]
+    )
+
+    useEffect(()=>{
+        Moralis.onAccountChanged((account)=>{
+            console.log(`Acc changed`)
+            if (account==null)
+            {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("null")
+
+            }
+        })
+    },[])
   
     return (
       <>
+       <div>
+    {account?(<div>Connected to your Metamask wallet: {account}</div>):(<button onClick={async ()=>{
+        await enableWeb3()
+        if (typeof window !== "undefined")
+        {
+            window.localStorage.setItem("connected","injected")
+        }
+        }}>Connect</button>)}
+
+    </div>
         <nav className="flex justify-between items-center py-4 px-8 bg-blue-500 text-white">
           <div>
             <a href="/" className="text-xl font-bold">Home</a>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import {SessionProvider} from "next-auth/react";
 import Navbar from "@/components/Navbar";
+import { MoralisProvider } from "react-moralis";
+import { createContext } from "react";
+
 
 export default function App({ Component, pageProps }) {
   return (
-  <SessionProvider session={pageProps.session}>
-    <Navbar />
-    <Component {...pageProps} />
-  </SessionProvider>
+  <MoralisProvider initializeOnMount={false}>
+    <SessionProvider session={pageProps.session}>
+      <Navbar/>
+      <Component {...pageProps} />
+    </SessionProvider>
+  </MoralisProvider>
   )
   // return <Component {...pageProps} />;
 }
