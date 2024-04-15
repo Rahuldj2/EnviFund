4282f25ab1f63e9cc9a416cf602f052d05bebb81
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-04-15 10:35:14 +0530

    Project Card styled and added hover

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -17,16 +17,14 @@ const ProjectCard = ({ project }) => {
     const progress = (funding_amount / funding_goal) * 100;
 
     return (
-        <div className={`flex flex-col relative bg-white pl-5 rounded-lg shadow-md mb-6 border-y-8 ${isCryptoProject ? "border-blue-500" : "border-yellow-500"} justify-start min-w-min`}>
-
+        <div className={`flex flex-col relative bg-white rounded-lg shadow-md mb-6 border-y-8 ${isCryptoProject ? "border-blue-500" : "border-yellow-500"} justify-start min-w-min cursor-pointer transition-transform hover:scale-105 transform-gpu hover:shadow-xl`}>
             <div className="flex items-center">
-                <h2 className={`text-[18px] leading-6 font-semibold ${isCryptoProject ? "text-blue-600" : "text-yellow-500"} text-balance text-left pl-4`}>
+                <h2 className={`text-lg font-semibold ${isCryptoProject ? "text-blue-600" : "text-yellow-500"} text-balance pl-4`}>
                     {project_title}
                 </h2>
-                <div className="w-min h-min relative flex justify-between">
-
+                <div className="flex items-center justify-center w-32 h-32 relative ml-auto mr-4">
                     <ProgressBar
-                        radius={25}
+                        radius={50}
                         progress={progress}
                         strokeWidth={8}
                         strokeColor={isCryptoProject ? "#2563eb" : "#eab308"}
@@ -34,14 +32,12 @@ const ProjectCard = ({ project }) => {
                         trackStrokeColor="#D1D5DB"
                         transition="1.5s"
                     />
-                    <div className="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 text-sm text-gray-800">
-                        {progress.toPrecision(2)}
+                    <div className="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 text-2xl text-gray-800">
+                        {progress.toPrecision(2)}%
                     </div>
-
                 </div>
-
             </div>
-            <div className="flex flex-col text-start mb-4 ml-4">
+            <div className="flex flex-col text-start mb-4 ml-5">
                 <p className={`text-sm font-bold text-gray-900`}>
                     Goal: <span className={`${funding_goal_reached ? "text-green-600" : "text-red-600"}`}>{funding_goal} {isCryptoProject ? "ETH" : "INR"}</span>
                 </p>
@@ -55,7 +51,6 @@ const ProjectCard = ({ project }) => {
                     alt={isCryptoProject ? "Ethereum Logo" : "Rupee Logo"}
                     layout="fill"
                     objectFit="cover"
-                // className="h-20 w-20"
                 />
             </div>
         </div>
